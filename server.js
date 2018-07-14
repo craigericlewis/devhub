72c107025d6b549c798799ac4d3bc84cd6b5e2eb
Craig Lewis <craigericlewis@gmail.com>
2018-07-14 15:26:00 -0400

    Initial Express Server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//DB config
+const db = require("./config/keys.js").mongoURI;
+
+// COnnect to mongoose
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+app.get("/", (req, res) => res.send("Hello"));
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
