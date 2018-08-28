132bc741b56d39706225e7cc9d93ff2574840fcc
Craig Lewis <craigericlewis@gmail.com>
2018-08-27 21:05:19 -0400

    Added Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
+import Navbar from "./components/layout/Navbar.js";
+import Footer from "./components/layout/Footer.js";
+import Landing from "./components/layout/Landing.js";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome to React</h1>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
