59ae680c3f138ed96fb93c4761a18fc4514a44d0
craigericlewis <craiglewis1508@yahoo.co.in>
2018-09-09 16:57:54 -0400

    Added Redux to the Register Page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import axios from "axios";
+import PropTypes from 'prop-types';
+import {withRouter} from 'react-router-dom';
 import classnames from "classnames";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
@@ -19,6 +20,12 @@ class Register extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps){
+    if(nextProps.errors){
+      this.setState({errors : nextProps.errors});
+    }
+  }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
@@ -33,12 +40,7 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    this.props.registerUser(newUser);
-
-    // axios
-    //   .post("/api/users/register", newUser)
-    //   .then(res => console.log(res.data))
-    //   .catch(err => this.setState({ errors: err.response.data }));
+    this.props.registerUser(newUser, this.props.history);
   }
 
   render() {
@@ -128,7 +130,18 @@ class Register extends Component {
   }
 }
 
+Register.propTypes = {
+  registerUser : PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors : PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth : state.auth,
+  errors : state.errors
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   { registerUser }
-)(Register);
+)(withRouter(Register));
