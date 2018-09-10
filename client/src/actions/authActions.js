f7a1dd80f5aebcde650d22337770e809b54b35e5
craigericlewis <craiglewis1508@yahoo.co.in>
2018-09-09 22:42:12 -0400

    State Changes

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,6 @@
 import { GET_ERRORS } from "./types";
 import axios from "axios";
+import setAuthToken from '../utils/setAuthToken';
 
 // Register User
 export const registerUser = (userData, history) => dispatch =>{
@@ -12,4 +13,19 @@ export const registerUser = (userData, history) => dispatch =>{
           payload : err.response.data
         })
       );
+};
+
+export const loginUser = (userData) => dispatch => {
+  axios.post('/api/users/login', userData)
+  .then(res => {
+    const {token} = res.data;
+    localStorage.setItem('jwtToken', token);
+    setAuthToken(token);
+  })
+  .catch(err => {
+    dispatch({
+      type: GET_ERRORS,
+      payload : err.response.data
+    })
+  });
 };
