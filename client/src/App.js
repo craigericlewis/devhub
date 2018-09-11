4e3d17eb505021eed5d161eae1a04011c1458660
craigericlewis <craiglewis1508@yahoo.co.in>
2018-09-10 22:03:37 -0400

    Private Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -15,6 +15,8 @@ import { logoutUser } from './actions/authActions';
 import Dashboard from './components/dashboard/Dashboard';
 import './App.css';
 import { clearCurrentProfile } from './actions/profileActions';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -48,7 +50,9 @@ class App extends Component {
 						<div className="container">
 							<Route exact path="/register" component={Register} />
 							<Route exact path="/login" component={Login} />
-							<Route exact path="/dashboard" component={Dashboard} />
+							<Switch>
+								<PrivateRoute exact path="/dashboard" component={Dashboard} />
+							</Switch>
 						</div>
 						<Footer />
 					</div>
