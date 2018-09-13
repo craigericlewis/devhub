f289b35baa68882c9e248898243d583c3a565334
craigericlewis <craiglewis1508@yahoo.co.in>
2018-09-13 17:05:50 -0400

    Preliminary Posts

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import NotFound from './components/not-found/NotFound';
-
+import Posts from './components/posts/Posts';
 // Check for token
 if (localStorage.jwtToken) {
 	//Set auth token header auth
@@ -72,6 +72,9 @@ class App extends Component {
 							</Switch>
 							<Switch>
 								<PrivateRoute exact path="/add-education" component={AddEducation} />
+							</Switch>
+							<Switch>
+								<PrivateRoute exact path="/feed" component={Posts} />
 							</Switch>
 							<Route exact path="/not-found" component={NotFound} />
 						</div>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import errorReducer from './errorReducer';
 import authReducer from './authReducer';
 import profileReducer from './profileReducer';
+import postReducer from './postReducer';
 
 export default combineReducers({
 	auth: authReducer,
 	errors: errorReducer,
 	profile: profileReducer,
+	post: postReducer,
 });
