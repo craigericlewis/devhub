6afdc86f5b02f5fea908d3f7a24d1d8309d535d0
craigericlewis <craiglewis1508@yahoo.co.in>
2018-09-17 18:44:08 -0400

    Fixed DevTools Error

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,19 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const middleware = [thunk];
 
 const initialState = {};
 
 const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+	rootReducer,
+	initialState,
+	// composeWithDevTools(
+	applyMiddleware(...middleware)
+	//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	// )
 );
 
 export default store;
